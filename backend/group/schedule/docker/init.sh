ebb7964e58841727a35de31c1c693b698b52ffda
isaac.hsu <isaac.hsu@vivatv.com.tw>
2026-03-12 13:46:09 +0800

    增加排程github ci 部屬到zeabur 流程

--- backend/group/schedule/docker/init.sh
+++ backend/group/schedule/docker/init.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#${JAR_NAME} ${JAVA_COMMAND_STR} 為deployment傳進來的環境變數
-java -server ${JAVA_COMMAND_STR} -jar  /opt/bigstock/${JAR_NAME}
-while sleep 60; do
-  ps aux |grep ${JAR_NAME} |grep -q -v grep
-  PROCESS_1_STATUS=$?
 
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
+java -server ${JAVA_COMMAND_STR} -jar /opt/bigstock/${JAR_NAME} &
+PID=$!
+
+while sleep 60; do
+  if ! kill -0 $PID 2>/dev/null; then
+    echo "Java process exited"
     exit 1
   fi
 done
