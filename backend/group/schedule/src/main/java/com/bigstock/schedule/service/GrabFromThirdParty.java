d7ad676b821f21103d4cd3619a318935af723eb5
isaac.hsu <isaac.hsu@vivatv.com.tw>
2026-03-20 11:43:26 +0800

    調整yahoo抓取融資券時間

--- backend/group/schedule/src/main/java/com/bigstock/schedule/service/GrabFromThirdParty.java
+++ backend/group/schedule/src/main/java/com/bigstock/schedule/service/GrabFromThirdParty.java
@@ -558,8 +558,8 @@ public class GrabFromThirdParty {
 		}
 	}
 
-	@PostConstruct
-	@Scheduled(cron = "0 40 21 * * ?", zone = "Asia/Taipei")
+//	@PostConstruct
+	@Scheduled(cron = "0 50 21 * * ?", zone = "Asia/Taipei")
 	public void grabMarginTradingAndShortSellingInfo() throws Exception
 	{
 		List<String> tpexStockCodes = stockInfoService.getStockCodeByStockType("0").stream().filter(data -> {
