a71a7e7bd0672ab767724b2010caffcc02cdd646
isaac.hsu <isaac.hsu@vivatv.com.tw>
2026-03-23 10:27:53 +0800

    調整yahoo抓取資券回填日期方式

--- backend/group/schedule/src/main/java/com/bigstock/schedule/service/GrabFromThirdParty.java
+++ backend/group/schedule/src/main/java/com/bigstock/schedule/service/GrabFromThirdParty.java
@@ -63,7 +63,7 @@ public class GrabFromThirdParty {
 	
 	private final MarginTradingAndShortSellingInfoService marginTradingAndShortSellingInfoService;
 	
-	@PostConstruct
+//	@PostConstruct
 	@Scheduled(cron = "0 40 15 * * ?", zone = "Asia/Taipei")
 	public void updateStockDayPriceByThirdParty() throws Exception {
 		List<String> tpexStockCodes = stockInfoService.getStockCodeByStockType("0").stream().filter(data -> {
@@ -558,7 +558,7 @@ public class GrabFromThirdParty {
 		}
 	}
 
-//	@PostConstruct
+	@PostConstruct
 	@Scheduled(cron = "0 50 21 * * ?", zone = "Asia/Taipei")
 	public void grabMarginTradingAndShortSellingInfo() throws Exception
 	{
